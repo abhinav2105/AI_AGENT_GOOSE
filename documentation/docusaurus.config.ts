bee2f63287bf79e4a1c276ba27d9bffabb9c3242
Jack Amadeo <jackamadeo@squareup.com>
2026-04-06 14:07:03 -0400

    fix baseUrl

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -21,7 +21,7 @@ const config: Config = {
   url: "https://block.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: process.env.TARGET_PATH || "/goose/",
+  baseUrl: process.env.TARGET_PATH || "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
